1cda627d5b1426c29822af6dd8e1ff5b8056f61f
sh1hsh1nk <13179671+sm86@users.noreply.github.com>
2023-10-16 18:12:29 -0400

    added osmosis

---
+++ data_utils/osmosis.py
@@ -0,0 +1,41 @@
+import requests
+import pandas as pd
+
+import utils
+
+
+class Osmosis:
+    URL = 'https://lcd-osmosis.keplr.app/staking/validators'
+
+    @classmethod
+    def get_validators(cls):
+        print('Retrieving data for Osmosis')
+        response = requests.get(cls.URL)
+
+        if response.status_code == 200:
+            data = response.json()
+            validators = data.get('result', [])
+
+            # Collecting specified values for each validator
+            validator_info_list = [
+                {
+                    'validator_id': validator.get('operator_address', 'Unknown'),
+                    'tokens': int(validator.get('tokens', '0')),  # Assuming 0 if tokens is not found,
+                    'name': validator.get('description', {}).get('moniker', 'Unknown')
+                }
+                for validator in validators
+            ]
+
+            # Creating a DataFrame
+            df = pd.DataFrame(validator_info_list)
+
+            # Sorting the DataFrame based on tokens (assumed to be numeric)
+            sorted_df = df.sort_values(by='tokens', ascending=False)
+            utils.write_csv(sorted_df, 'osmosis')
+            return sorted_df
+        else:
+            print(f'Failed to retrieve data: {response.status_code}')
+            return None
+
+if __name__ == '__main__':
+    validator_dataframe = Osmosis.get_validators()
