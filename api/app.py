e94d86438ce66dbe441a68dcf2201dcb23fd863f
sh1hsh1nk <13179671+sm86@users.noreply.github.com>
2024-01-25 22:34:42 -0500

    added date to API metrics

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 import sys
 from datetime import datetime,timedelta
 from pathlib import Path
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 # Add the project root directory to sys.path
@@ -12,13 +12,19 @@ from metrics.decentralizationMetrics import DecentralizationMetrics
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/metrics')
-def hello_world():
-    date = datetime.now() - timedelta(days=1)
-    date= date.strftime('%d%m%Y')
-    print(date)
+@app.route('/metrics/<date>')
+def metrics(date):
     result = DecentralizationMetrics.calculate_metrics(date)
+
+    # Check if result is empty
+    if result.empty:
+        today = datetime.now().strftime('%d-%m-%Y')
+        return jsonify({
+            "error": "No data available for the selected date. Please select a date between 24-10-2023 and " + today
+        }), 400
+
     return result.to_json(orient='records')
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
